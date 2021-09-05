deb07d7c01b59b8825b479d5f552da0e5a53de79
job karani <job.nyambura@student.moringaschool.com>
2021-09-05 22:50:23 +0300

    adds an eventlistener to the submit button that console logs the word hey

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@ window.addEventListener("DOMContentLoaded", function(){
     let maleGender = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
     let femaleGender = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
+    inputForm.addEventListener("submit", function(){
+        console.log("hey");
     
+        
+    })
 })
 
