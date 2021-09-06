f7b39cb67f86be7edb1c7c9e424cf4fc4789bffa
job karani <job.nyambura@student.moringaschool.com>
2021-09-06 11:53:16 +0300

    adds a know.html file

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ window.addEventListener("DOMContentLoaded", function(){
 
         let date = new Date(dateInput);
         let day = date.getDay();
-        alert(maleGender[day]);
+        // alert(maleGender[day]);
 
         let genderInput =  document.querySelector("#gender").value;
         console.log(genderInput);
